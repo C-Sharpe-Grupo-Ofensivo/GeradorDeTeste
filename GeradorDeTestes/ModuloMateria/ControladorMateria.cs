f9fbce189027ea0f5b8a89ec870f9fa2d2618847
tomat <tomat@DESKTOP-0VULJ7N>
2023-07-04 22:58:00 -0300

    Controlador materia

--- GeradorDeTestes/ModuloMateria/ControladorMateria.cs
+++ GeradorDeTestes/ModuloMateria/ControladorMateria.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.WinApp.ModuloDisciplina;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,102 @@ using System.Threading.Tasks;
 
 namespace GeradorDeTestes.WinApp.ModuloMateria
 {
-    public class ControladorMateria
+    public class ControladorMateria : ControladorBase
     {
+        private TabelaMateriaControl tabelaMateria;
+        private IRepositorioMateria repositorioMateria;
+
+        public ControladorMateria(IRepositorioMateria repositorioDisciplina)
+        {
+            this.repositorioDisciplina = repositorioDisciplina;
+        }
+        public override string ToolTipInserir { get { return "Inserir nova disciplina"; } }
+
+        public override string ToolTipEditar { get { return "Editar disciplina existente"; } }
+
+        public override string ToolTipExcluir { get { return "Excluir disciplina existente"; } }
+
+        public override void Editar()
+        {
+            Disciplina Disciplina = ObterDisciplinaSelecionado();
+
+            if (Disciplina == null)
+            {
+                MessageBox.Show($"Selecione um cliente primeiro!",
+                    "Edição de Clientes",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+
+                return;
+            }
+        }
+
+        public override void Excluir()
+        {
+            Disciplina disciplina = ObterDisciplinaSelecionado();
+
+            if (disciplina == null)
+            {
+                MessageBox.Show($"Selecione uma disciplina primeiro!",
+                    "Exclusão de uma Disciplina",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
+            DialogResult opcaoEscolhida = MessageBox.Show($"Deseja excluir o disciplina {disciplina.nome}?", "Exclusão de Disciplina",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                repositorioDisciplina.Excluir(disciplina);
+            }
+
+            CarregarDisciplina();
+        }
+
+        public override void Inserir()
+        {
+            TelaDisciplinaForm telaDisciplina = new TelaDisciplinaForm();
+
+            DialogResult opcaoEscolhida = telaDisciplina.ShowDialog();
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Disciplina disciplina = telaDisciplina.ObterDisciplina();
+
+                repositorioDisciplina.Inserir(disciplina);
+            }
+            CarregarDisciplina();
+        }
+        private void CarregarDisciplina()
+        {
+            List<Disciplina> Disciplina = repositorioDisciplina.SelecionarTodos();
+
+            tabelaDisciplina.AtualizarRegistros(Disciplina);
+        }
+
+        public override UserControl ObterListagem()
+        {
+            if (tabelaDisciplina == null)
+                tabelaDisciplina = new TabelaDisciplinaControl();
+
+            CarregarDisciplina();
+
+            return tabelaDisciplina;
+        }
+
+        public override string ObterTipoCadastro()
+        {
+            return "Cadastro de Disciplina";
+        }
+        private Disciplina ObterDisciplinaSelecionado()
+        {
+            int id = tabelaDisciplina.ObterIdSelecionado();
+
+            return repositorioDisciplina.SelecionarPorId(id);
+        }
     }
 }
