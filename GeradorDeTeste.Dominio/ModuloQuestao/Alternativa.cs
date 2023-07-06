2eb12d5aa88d4ebd4ae932d5365444b17239db59
Arthur Leonardo Filipus <ArthurFilipus@hotmail.com>
2023-07-06 14:51:49 -0300

    Criada Classe Alternativa

--- GeradorDeTeste.Dominio/ModuloQuestao/Alternativa.cs
+++ GeradorDeTeste.Dominio/ModuloQuestao/Alternativa.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace GeradorDeTestes.Dominio.ModuloQuestao
 {
-    internal class Alternativa
+    public class Alternativa : EntidadeBase<Alternativa>
     {
+        public string alternativa { get; set; }
+        public Questao questao { get; set; }
+        public bool alternativaCorreta { get; set; }
+        public Alternativa() { }
+        public Alternativa(string alternativa, Questao questao) 
+        {
+            this.alternativa = alternativa;
+            this.questao = questao;
+            alternativaCorreta = false;
+        }
+        
+        public override void AtualizarInformacoes(Alternativa registroAtualizado)
+        {
+            this.alternativa = registroAtualizado.alternativa;
+            this.alternativaCorreta = registroAtualizado.alternativaCorreta;
+        }
+
+        public override string[] Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if (alternativa == null)
+                erros.Add("O campo 'Alternativa' é obrigatório");
+
+            return erros.ToArray();
+        }
+        public override string ToString()
+        {
+            return alternativa.ToString();
+        }
     }
 }
