c17562e7f7f66f4e520ffc835058d8c2c9c1f057
Anderson Braga <anderson.braga@uniplaclages.edu.br>
2023-07-07 14:52:29 -0300

    Repostiorio

--- GeradorDeTeste.Infra.Sql/ModuloMateria/RepositorioMateriaEmSql.cs
+++ GeradorDeTeste.Infra.Sql/ModuloMateria/RepositorioMateriaEmSql.cs
@@ -8,70 +8,70 @@ using System.Threading.Tasks;
 
 namespace GeradorDeTeste.Infra.Sql.ModuloMateria
 {
-    public class RepositorioMateriaEmSql : RepositorioEmSqlBase<Materia, MapeadorMateria>, IRepositorioMateria
-    {
-        protected override string sqlInserir => @"INSERT INTO [DBO]. [TBMATERIA]
-                                                    (
-                                                      [NOME]
-                                                      ,[DISCIPLINA_ID]
-                                                      ,[SERIE]
-                                                    )
-                                                    VALUES
-                                                    (
-                                                       @NOME
-                                                      ,@DISCIPLINA_ID
-                                                      ,@SERIE
-                                                    );
-                                                    SELECT SCOPE_IDENTITY();";
+	public class RepositorioMateriaEmSql : RepositorioEmSqlBase<Materia, MapeadorMateria>, IRepositorioMateria
+	{
+		protected override string sqlInserir => @"INSERT INTO [DBO]. [TBMATERIA]
+													(
+													  [NOME]
+													  ,[DISCIPLINA_ID]
+													  ,[SERIE]
+													)
+													VALUES
+													(
+													   @NOME
+													  ,@DISCIPLINA_ID
+													  ,@SERIE
+													);
+													SELECT SCOPE_IDENTITY();";
 
 
-        protected override string sqlEditar => @"UPDATE[TBMATERIA]
-                                               SET
-                                                   [NOME] = @NOME
-                                                  ,[DISCIPLINA_ID] = @DISCIPLINA_ID
-                                                  ,[SERIE] = @SERIE
-                                             WHERE [ID] = @ID;";
+		protected override string sqlEditar => @"UPDATE[TBMATERIA]
+											   SET
+												   [NOME] = @NOME
+												  ,[DISCIPLINA_ID] = @DISCIPLINA_ID
+												  ,[SERIE] = @SERIE
+											 WHERE [ID] = @ID;";
 
-        protected override string sqlExcluir => @"DELETE FROM [TBMATERIA]
-	                                                WHERE 
-		                                                [ID] = @ID";
+		protected override string sqlExcluir => @"DELETE FROM [TBMATERIA]
+													WHERE 
+														[ID] = @ID";
 
-        protected override string sqlSelecionarTodos => @"SELECT 
-	                                                        M.[ID]              MATERIA_ID 
-	                                                       ,M.[NOME]            MATERIA_NOME
-	                                                       ,M.[DISCIPLINA_ID]   DISCIPLINA_ID
-                                                           ,M.[SERIE]           MATERIA_SERIE
-                                                           ,D.[NOME]            DISCIPLINA_NOME
-                                                        FROM 
-	                                                        [TBMATERIA] AS M
-                                                        INNER JOIN [DISCIPLINA] AS D
-                                                                ON M.[DISCIPLINA_ID] = D.ID";
+		protected override string sqlSelecionarTodos => @"SELECT 
+															M.[ID]              MATERIA_ID 
+														   ,M.[NOME]            MATERIA_NOME
+														   ,M.[DISCIPLINA_ID]   DISCIPLINA_ID
+														   ,M.[SERIE]           MATERIA_SERIE
+														   ,D.[NOME]            DISCIPLINA_NOME
+														FROM 
+															[TBMATERIA] AS M
+														INNER JOIN [DISCIPLINA] AS D
+																ON M.[DISCIPLINA_ID] = D.ID";
 
-        protected override string sqlSelecionarPorId => @"SELECT 
-	                                                    M.[ID]             MATERIA_ID 
-	                                                   ,M.[NOME]           MATERIA_NOME
-	                                                   ,M.[DISCIPLINA_ID]  DISCIPLINA_ID
-                                                       ,M.[SERIE]          MATERIA_SERIE
-                                                       ,D.[NOME]           DISCIPLINA_NOME
-                                                    FROM 
-	                                                    [TBMATERIA] AS M
-                                                    INNER JOIN [DISCIPLINA] AS D
-                                                            ON M.[DISCIPLINA_ID] = D.ID
-                                                    WHERE 
-                                                        M.[ID] = @ID";
+		protected override string sqlSelecionarPorId => @"SELECT 
+														M.[ID]             MATERIA_ID 
+													   ,M.[NOME]           MATERIA_NOME
+													   ,M.[DISCIPLINA_ID]  DISCIPLINA_ID
+													   ,M.[SERIE]          MATERIA_SERIE
+													   ,D.[NOME]           DISCIPLINA_NOME
+													FROM 
+														[TBMATERIA] AS M
+													INNER JOIN [DISCIPLINA] AS D
+															ON M.[DISCIPLINA_ID] = D.ID
+													WHERE 
+														M.[ID] = @ID";
 
-        public override List<Materia> SelecionarTodos()
-        {
-            List<Materia> materias = base.SelecionarTodos();
+		public override List<Materia> SelecionarTodos()
+		{
+			List<Materia> materias = base.SelecionarTodos();
 
-            return materias;
-        }
+			return materias;
+		}
 
-        public override Materia SelecionarPorId(int id)
-        {
-            Materia materia = base.SelecionarPorId(id);
+		public override Materia SelecionarPorId(int id)
+		{
+			Materia materia = base.SelecionarPorId(id);
 
-            return materia;
-        }
-    }
+			return materia;
+		}
+	}
 }
