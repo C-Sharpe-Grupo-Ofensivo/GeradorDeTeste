9752c4b87dc000320d97eb6a7b2a1441d468ded4
tomat <tomat@DESKTOP-0VULJ7N>
2023-07-07 14:51:08 -0300

    Banco de dados

---
+++ Database1/dbo/Tables/TBMATERIA.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TBMATERIA] (
+    [id]            INT           IDENTITY (1, 1) NOT NULL,
+    [nome]          VARCHAR (MAX) NOT NULL,
+    [serie]         VARCHAR (MAX) NOT NULL,
+    [disciplina_id] INT           NOT NULL,
+    CONSTRAINT [PK_tbmateria] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_tbMateria_tbdisciplina] FOREIGN KEY ([disciplina_id]) REFERENCES [dbo].[TBDISCIPLINA] ([id])
+);
+
