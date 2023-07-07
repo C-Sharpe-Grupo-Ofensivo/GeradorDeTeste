1449d18fd58c26af62e5a7ad185dd9e01e06c785
Anderson Braga <anderson.braga@uniplaclages.edu.br>
2023-07-07 14:51:47 -0300

    Mapeador e Repositorio materia

--- GeradorDeTeste.Infra.Sql/ModuloMateria/MapeadorMateria.cs
+++ GeradorDeTeste.Infra.Sql/ModuloMateria/MapeadorMateria.cs
@@ -1,17 +1,34 @@
-﻿using GeradorDeTestes.Dominio.ModuloMateria;
+﻿using GeradorDeTeste.Infra.Sql.Compartilhado;
+using GeradorDeTeste.Infra.Sql.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
 using Microsoft.Data.SqlClient;
 
 namespace GeradorDeTeste.Infra.Sql.ModuloMateria
 {
-    internal class MapeadorMateria
+    public class MapeadorMateria : MapeadorBase<Materia>
     {
-        public MapeadorMateria()
+     
+
+        public override void ConfigurarParametros(SqlCommand comando, Materia registro)
         {
+            comando.Parameters.AddWithValue("@ID", registro.id);
+            comando.Parameters.AddWithValue("@NOME", registro.nome);
+            comando.Parameters.AddWithValue("@SERIE", registro.serie);
+            comando.Parameters.AddWithValue("@DISCIPLINA_ID", registro.disciplina);
         }
 
-        internal Materia ConverterRegistro(SqlDataReader leitorRegistros)
+        public override Materia ConverterRegistro(SqlDataReader leitorRegistros)
         {
-            throw new NotImplementedException();
+            int id = Convert.ToInt32(leitorRegistros["MATERIA_ID"]);
+            string nome = Convert.ToString(leitorRegistros["MATERIA_NOME"]);
+            string serie = Convert.ToString(leitorRegistros["SERIE"]);
+
+            Disciplina disciplina = new MapeadorDisciplina().ConverterRegistro(leitorRegistros);
+
+            return new Materia(id, nome, disciplina, serie);
         }
+
+    
     }
 }
