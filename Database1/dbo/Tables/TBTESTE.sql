a52bb56645427c22b83f94940228b3857d5e24c0
tomat <tomat@DESKTOP-0VULJ7N>
2023-07-10 14:05:37 -0300

    Banco de dados teste

---
+++ Database1/dbo/Tables/TBTESTE.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBTESTE] (
+    [id]                INT          IDENTITY (1, 1) NOT NULL,
+    [nome]              VARCHAR (50) NOT NULL,
+    [disciplina_id]     INT          NOT NULL,
+    [materia_id]        INT          NOT NULL,
+    [quantidadequestao] INT          NOT NULL,
+    [recuperacao]       BIT          NOT NULL,
+    CONSTRAINT [PK_TBTESTE] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
